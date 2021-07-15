52be45f7c6f5617d0075a2b341d6774e4a5f3235
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-15 14:25:12 -0300

    created test int array class

--- src/main/kotlin/me/lucas/diobootcamp/collections/TesteIntArray.kt
+++ src/main/kotlin/me/lucas/diobootcamp/collections/TesteIntArray.kt
@@ -1,2 +1,38 @@
 package me.lucas.diobootcamp.collections
 
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 8
+    values[2] = 5
+    values[3] = 3
+    values[4] = 9
+
+    for (value in values) {
+        println(value)
+    }
+
+    printSection()
+    values.forEach {
+        println(it)
+    }
+
+    printSection()
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    printSection()
+    for (index in values.indices) {
+        println(values[index])
+    }
+
+    printSection()
+    values.sort()
+    for (value in values) {
+        println(value)
+    }
+}
+
+fun printSection() = println("\n===================================")
