5fbfa8ab4cfe9620db1c3934f5446d75d7cc555f
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-16 14:53:48 -0300

    created set test class

---
+++ src/main/kotlin/me/lucas/diobootcamp/collections/TesteSet.kt
@@ -0,0 +1,24 @@
+package me.lucas.diobootcamp.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios1 = setOf(joao, pedro)
+    val funcionarios2 = setOf(maria)
+    val funcionarios3 = setOf(joao, pedro, maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach(::println)
+
+    printSection()
+    val resultSubtract = funcionarios3.subtract(funcionarios1)
+    resultSubtract.forEach(::println)
+
+    printSection()
+    val resultIntersect = funcionarios1.intersect(funcionarios3)
+    resultIntersect.forEach(::println)
+    println("Teste intersecção vazia: ${funcionarios1.intersect(funcionarios2)}")
+
+}
