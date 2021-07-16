aa0c49bdb7f0d1fc7840d8f26f4873a38fcc22c0
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-16 14:30:19 -0300

    created list of test class

---
+++ src/main/kotlin/me/lucas/diobootcamp/collections/TesteList.kt
@@ -0,0 +1,37 @@
+package me.lucas.diobootcamp.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+    funcionarios.forEach(::println)
+
+    printSection()
+    println(funcionarios.find{ it.nome == "Maria" })
+
+    printSection()
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach(::println)
+
+    printSection()
+    funcionarios
+        .groupBy { it.tipoContrato }
+        .forEach(::println)
+
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContrato: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salario: $${String.format("%.2f", salario)}
+        """.trimIndent()
+}
